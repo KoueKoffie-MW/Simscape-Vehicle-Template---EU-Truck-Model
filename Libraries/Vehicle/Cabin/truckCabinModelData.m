9d1c72d5a27a9adeb36f9eb48b0fd68b2af8bd3d
lnicolet <lnicolet@MATHWORKS-EbjiK>
2024-11-14 17:20:56 +0100

    minor changes

--- Libraries/Vehicle/Cabin/truckCabinModelData.m
+++ Libraries/Vehicle/Cabin/truckCabinModelData.m
@@ -22,10 +22,10 @@ cabin_input.acc_x = data.se_imu_cabin_xsens_mti_680g_can_tego.m_oEgoDynamic_f64A
 cabin_input.acc_y = data.se_imu_cabin_xsens_mti_680g_can_tego.m_oEgoDynamic_f64Ay;
 cabin_input.acc_z = data.se_imu_cabin_xsens_mti_680g_can_tego.m_oEgoDynamic_f64Az;
 
+% Pitch and Roll angles in rad
 cabin_reference.time = (data.se_adma_cabin_tego.timestamp - data.se_adma_cabin_tego.timestamp(1)) * 1e-9;
 cabin_reference.roll = data.se_adma_cabin_tego.m_oEgoDynamic_f64Roll;
 cabin_reference.pitch = data.se_adma_cabin_tego.m_oEgoDynamic_f64Pitch;
-
 chassis_reference.time = (data.se_oxts_rt3k_ego.timestamp - data.se_oxts_rt3k_ego.timestamp(1))*1e-9;
 chassis_reference.roll = data.se_oxts_rt3k_ego.m_oEgoDynamic_f64Roll;
 chassis_reference.pitch = data.se_oxts_rt3k_ego.m_oEgoDynamic_f64Pitch;
